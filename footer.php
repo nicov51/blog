62befbda14f525bda5ab00e4bdfa0f3547ace5f1
nicov51 <nicolas.viennot@laposte.net>
2025-02-13 13:07:49 +0100

    initial commit

---
+++ footer.php
@@ -0,0 +1,5 @@
+<footer class="bg-light text-center text-lg-start mt-5">
+    <div class="text-center p-3">
+        © 2023 Mon Blog
+    </div>
+</footer>
---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mon Blog</title>
+
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+
+    <link href="css/style.css" rel="stylesheet">
+</head>
+<body>
+<?php include 'header.php'; ?>
+
+<div class="container mt-5">
+    <h1>Bienvenue sur mon blog !</h1>
+    <p>Ceci est une page d'accueil de test pour mon projet de blog.</p>
+</div>
+
+<?php include 'footer.php'; ?>
+
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+<script src="js/script.js"></script>
+</body>
+</html>
